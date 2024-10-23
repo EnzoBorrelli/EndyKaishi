0b47b3d77abbf91e1118099f0443bc846c0d5c61
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-10-23 19:05:11 -0300

    added socials

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,9 +1,32 @@
+"use client";
 import React from "react";
+import { Socials } from "./socials";
+import { motion } from "framer-motion";
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-between w-full h-20 px-8 py-4 bg-brand-400 ">
-      Footer
+    <footer className="items-center justify-between w-full px-8 py-4 md:flex bg-brand-400 ">
+      <h2 className="mb-4 md:text-center md:mb-0 md:text-lg text-text-secondary">
+        © 2024 Endy Kaishi® by Enzo Borrelli
+      </h2>
+      <div className="flex items-center gap-6 text-lg md:justify-center md:text-3xl">
+        {Socials.map((social) => (
+          <motion.a
+            whileHover={{
+              y: -8,
+              scale: 1.2,
+              transition: { duration: 0.2, type: "spring" },
+            }}
+            className={`p-2 rounded-full ring-2 ring-text-secondary ${social.color}`}
+            key={social.name}
+            href={social.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <social.icon />
+          </motion.a>
+        ))}
+      </div>
     </footer>
   );
 }
