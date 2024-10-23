ffae93c213d3bb49b5d4d4117856fe216bfbe289
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-10-23 17:00:12 -0300

    Update template.tsx

--- app/template.tsx
+++ app/template.tsx
@@ -8,8 +8,8 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <motion.div
         style={{
           position: "absolute",
-          width: "100vw",
-          height: "100vh",
+          width: "100dvw",
+          height: "100dvh",
           display: "flex",
           zIndex: "20",
           overflow:"hidden"
@@ -18,44 +18,44 @@ export default function Template({ children }: { children: React.ReactNode }) {
         <motion.div
           style={{
             position: "absolute",
-            width: "100vw",
-            height: "100vh",
+            width: "100dvw",
+            height: "100dvh",
             scale:"1.5",
             backgroundColor: "rgb(0, 222, 204)",
           }}
-          initial={{ x: "-100vw", y: "100vh", rotate: 45 }}
-          animate={{ x: "100vw", y: "-100vh", rotate: 45 }}
+          initial={{ x: "-100dvw", y: "100dvh", rotate: 45 }}
+          animate={{ x: "100dvw", y: "-150dvh", rotate: 45 }}
           transition={{ ease: "easeInOut", duration: 0.5 }} // Smooth transition
         ></motion.div>
         <motion.div
           style={{
             position: "absolute",
-            width: "100vw",
-            height: "100vh",
+            width: "100dvw",
+            height: "100dvh",
             scale:"1.5",
             backgroundColor: "rgb(10, 49, 64)",
           }}
-          initial={{ x: "-100vw", y: "100vh", rotate: 45 }}
-          animate={{ x: "100vw", y: "-100vh", rotate: 45 }}
+          initial={{ x: "-100dvw", y: "100dvh", rotate: 45 }}
+          animate={{ x: "100dvw", y: "-150dvh", rotate: 45 }}
           transition={{ ease: "easeInOut", duration: 0.5, delay: 0.1 }}
         ></motion.div>
         <motion.div
           style={{
             position: "absolute",
-            width: "100vw",
-            height: "100vh",
+            width: "100dvw",
+            height: "100dvh",
             scale:"1.5",
             backgroundColor: "rgb(22, 128, 140)",
           }}
-          initial={{ x: "-100vw", y: "100vh", rotate: 45 }}
-          animate={{ x: "100vw", y: "-100vh", rotate: 45 }}
+          initial={{ x: "-100dvw", y: "100dvh", rotate: 45 }}
+          animate={{ x: "100dvw", y: "-150dvh", rotate: 45 }}
           transition={{ ease: "easeInOut", duration: 0.5, delay: 0.2 }}
         ></motion.div>
       </motion.div>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ ease: "easeIn", duration: 1.5 }}
+        transition={{ ease: "easeIn", duration: 1.7 }}
       >
         {children}
       </motion.div>
