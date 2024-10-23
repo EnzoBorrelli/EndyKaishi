4eb3fd0c93a98c207030ad8072d546f80d0b2eaf
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-10-23 19:04:59 -0300

    Create socials.ts

---
+++ components/footer/socials.ts
@@ -0,0 +1,48 @@
+import {
+  FaLinkedinIn,
+  FaInstagram,
+  FaYoutube,
+  FaDiscord,
+  FaGithub,
+  FaXTwitter,
+} from "react-icons/fa6";
+
+export const Socials = [
+  {
+    name: "Twitter",
+    icon: FaXTwitter,
+    url: "https://x.com/Endy_Kaishi",
+    color: "text-white bg-black",
+  },
+  {
+    name: "Instagram",
+    icon: FaInstagram,
+    url: "https://www.instagram.com/endy_debug_diaries/",
+    color:
+      "text-white bg-gradient-to-tr from-yellow-400 via-pink-500 to-purple-600",
+  },
+  {
+    name: "Youtube",
+    icon: FaYoutube,
+    url: "https://www.youtube.com/@Endy.Debug.Diaries",
+    color: "text-white bg-red-600",
+  },
+  {
+    name: "GitHub",
+    icon: FaGithub,
+    url: "https://github.com/EnzoBorrelli",
+    color: "text-white bg-black",
+  },
+  {
+    name: "Linkedin",
+    icon: FaLinkedinIn,
+    url: "https://linkedin.com/in/enzoborrelli",
+    color: "text-white bg-sky-600",
+  },
+  {
+    name: "Discord",
+    icon: FaDiscord,
+    url: "https://discord.gg/2ATQkaypqQ",
+    color: "text-white bg-indigo-900",
+  },
+];
