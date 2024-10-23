7bfca4b6d45692ebf9ce1d54afb959574e0e9ad9
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-10-23 13:32:44 -0300

    added custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,22 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        brand: {
+          100: "rgb(var(--color-brand100)/<alpha-value>)",
+          200: "rgb(var(--color-brand200)/<alpha-value>)",
+          300: "rgb(var(--color-brand300)/<alpha-value>)",
+          400: "rgb(var(--color-brand400)/<alpha-value>)",
+          500: "rgb(var(--color-brand500)/<alpha-value>)",
+        },
+        text: {
+          primary: "rgb(var(--color-text-primary)/<alpha-value>)",
+          secondary: "rgb(var(--color-text-secondary)/<alpha-value>)",
+          disabled: "rgb(var(--color-text-disabled)/<alpha-value>)",
+        },
+        accent:{
+          1: "rgb(var(--color-accent1)/<alpha-value>)",
+          2: "rgb(var(--color-accent2)/<alpha-value>)",
+        }
       },
     },
   },
