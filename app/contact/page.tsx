a2b7f3548f2e1a6f9124d7bd6e3c104544b3702f
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-10-28 19:16:11 -0300

    Delete page.tsx

--- app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Contact() {
-  return (
-    <main className="pt-20 font-bold text-center bg-brand-500 w-full h-dvh">
-      <h2 className="mb-10 text-4xl text-text-primary">Contact</h2>
-    </main>
-  )
-}
