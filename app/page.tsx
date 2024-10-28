b8d6910bc59cd230a56352f65f4e1040bf5586f5
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-10-28 19:16:37 -0300

    started homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,55 @@
+import { Socials } from "@/components/footer/socials";
+import Projects from "@/components/home/projects";
+import { skills } from "@/components/home/skills";
+import { ProjectsData } from "@/components/home/projectsdata";
+import AnimatedText from "@/components/home/animatedText";
+
 export default function Home() {
   return (
-    <main className="w-full pt-20 font-bold text-center bg-brand-500 h-dvh">
-      <h2 className="mb-10 text-4xl text-text-primary">Primary text</h2>
-      <h2 className="mb-10 text-2xl text-text-secondary">Secondary text</h2>
-      <h2 className="mb-10 text-lg text-text-disabled">disabled text</h2>
-      <div className="flex items-center justify-center w-full gap-8 mt-120">
-        <div className="size-24 bg-brand-400 ring-8 ring-black"></div>
-        <div className="size-24 bg-brand-300 ring-8 ring-black"></div>
-        <div className="size-24 bg-brand-200 ring-8 ring-black"></div>
-        <div className="size-24 bg-brand-100 ring-8 ring-black"></div>
-        <div className="size-24 bg-accent-1 ring-8 ring-black"></div>
-        <div className="size-24 bg-accent-2 ring-8 ring-black"></div>
-      </div>
+    <main className="px-10 py-20 text-center size-full bg-brand-500 text-text-primary">
+      <section className="flex justify-center gap-2 px-10 py-4 bg-red-400">
+        <div className="w-1/3 text-left bg-blue-500">
+          <AnimatedText text="Endy Kaishi®"/>
+          <p className="text-lg">
+            a frontend developer specialized in react and nextjs, along wth
+            typescript
+          </p>
+          <h4 className="text-sm text-text-secondary">
+            Located in Buenos Aires, Argentina
+          </h4>
+          <div className="flex items-center justify-start gap-4 mt-2 bg-green-300">
+            {Socials.map((social) => (
+              <a
+                className="p-1 rounded-md bg-brand-400 ring-2 ring-text-secondary"
+                key={social.name}
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <social.icon />
+              </a>
+            ))}
+          </div>
+        </div>
+        <div className="grid grid-cols-4 gap-4 mt-2 bg-orange-300">
+          {skills.map((skill) => (
+            <div
+              className="flex items-center justify-center p-1 text-3xl rounded-md size-10 bg-brand-400 "
+              key={skill.name}
+            >
+              <skill.icon />
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="flex-col justify-center gap-2 px-10 py-4 mt-2">
+        <h2 className="my-4 text-2xl font-bold">My recent Work</h2>
+        <div className="grid grid-cols-3">
+          {ProjectsData.map((project, index) => (
+            <Projects projectData={project} delay={index} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
