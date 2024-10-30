3f7afe9248a8758cdb2cc35ebe67d3adf89f7291
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-10-30 09:43:16 -0300

    Update skills.ts

--- components/home/skills.ts
+++ components/home/skills.ts
@@ -1,54 +1,49 @@
 import { IconType } from "react-icons";
 import { FaReact } from "react-icons/fa6";
 import { RiSupabaseLine } from "react-icons/ri";
-import { SiFramer, SiTailwindcss} from "react-icons/si";
-import { TbBrandTypescript, TbBrandNextjs, TbBrandPrisma } from "react-icons/tb";
+import { SiFramer, SiTailwindcss } from "react-icons/si";
+import {
+  TbBrandTypescript,
+  TbBrandNextjs,
+  TbBrandPrisma,
+} from "react-icons/tb";
 import { VscVscode } from "react-icons/vsc";
 
-interface Skill  {
-    name:string,
-    icon: IconType,
-    isRemarkable:boolean
+interface Skill {
+  name: string;
+  icon: IconType;
 }
-export const skills : Skill[] = [
+export const skills: Skill[] = [
   {
     name: "typescript",
     icon: TbBrandTypescript,
-    isRemarkable: true,
   },
   {
     name: "react",
     icon: FaReact,
-    isRemarkable: true,
   },
   {
     name: "nextjs",
     icon: TbBrandNextjs,
-    isRemarkable: true,
   },
   {
     name: "prisma",
     icon: TbBrandPrisma,
-    isRemarkable: true,
   },
   {
     name: "supabase",
     icon: RiSupabaseLine,
-    isRemarkable: false,
   },
   {
     name: "tailwind",
     icon: SiTailwindcss,
-    isRemarkable: false,
   },
   {
     name: "framer",
     icon: SiFramer,
-    isRemarkable: false,
   },
   {
     name: "vscode",
     icon: VscVscode,
-    isRemarkable: false,
   },
 ];
