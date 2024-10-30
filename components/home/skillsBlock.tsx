b8ce16cb14220d9acb54a1201475b2bc43c63cd8
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-10-30 10:10:54 -0300

    added animation

--- components/home/skillsBlock.tsx
+++ components/home/skillsBlock.tsx
@@ -1,17 +1,50 @@
-import React from 'react'
-import { skills } from './skills'
+"use client";
+import {
+  domAnimation,
+  LazyMotion,
+  m,
+} from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { skills } from "./skills";
+
+const sineWaveVariant = (index:number) => ({
+  animate: {
+    y: [0, -5, 0, 5, 0], // Creates a sine wave effect
+    transition: {
+      duration: 5, // Adjust duration to control speed
+      ease: "linear",
+      repeat: Infinity, // Loop indefinitely
+      delay: index * 0.2, // Offset each skill to create a wave
+    },
+  },
+});
 
 export default function SkillsBlock() {
+  const [animateKey, setAnimateKey] = useState(0);
+
+  useEffect(() => {
+    // Update key to re-trigger animations on each page load
+    setAnimateKey((prevKey) => prevKey + 1);
+  }, []);
+
   return (
-    <div className="grid grid-cols-4 gap-4 mt-2 bg-blue-400">
-          {skills.map((skill) => (
-            <div
-              className="flex items-center justify-center p-1 text-3xl rounded-md size-10 bg-brand-400 "
-              key={skill.name}
-            >
-              <skill.icon />
-            </div>
-          ))}
-        </div>
-  )
+    <LazyMotion features={domAnimation}>
+      <m.div
+        key={animateKey} // Using a unique key to reset animation
+        initial={null}
+        animate="animate"
+        className="grid grid-cols-4 gap-4 mt-2 bg-blue-400"
+      >
+        {skills.map((skill, index) => (
+          <m.div
+            key={skill.name}
+            variants={sineWaveVariant(index)}
+            className="flex items-center justify-center p-1 text-3xl rounded-md size-10 bg-brand-400"
+          >
+            <skill.icon />
+          </m.div>
+        ))}
+      </m.div>
+    </LazyMotion>
+  );
 }
