43cbdfb57ec69f881aa28443c1b4b76c84005304
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-06-29 09:57:42 -0300

    deleted unused routes

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function About() {
-  return (
-    <main className="pt-20 font-bold text-center bg-brand-500 w-full h-dvh">
-      <h2 className="mb-10 text-4xl text-text-primary">About</h2>
-    </main>
-  )
-}
--- app/portfolio/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Portfolio() {
-  return (
-    <main className="w-full pt-20 font-bold text-center bg-brand-500 h-dvh">
-      <h2 className="mb-10 text-4xl text-text-primary">Portfolio</h2>
-    </main>
-  )
-}
